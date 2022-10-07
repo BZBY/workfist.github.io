2a05e41d65292205c405ce830cf42713144935f1
AmlyC <43779380+BZBY@users.noreply.github.com>
2022-10-07 22:31:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function Convert(i,j){
+
+    document.getElementById(i).innerHTML = marked.parse(document.getElementById(i).value)
+
+}
